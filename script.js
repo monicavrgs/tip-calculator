b342cb4f260626b55392d90c96e225d7b6c61730
Mônica Vargas <monicavmsalvador@gmail.com>
2021-10-26 15:27:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function getTip(event){
+    let bill = document.getElementById('bill-input').value
+    let percentage = Number(event.target.value * 0.01)
+    let amountOfPeople = Number(document.getElementById('number-of-people').value)
+
+    if(bill.includes(',')){
+        bill = bill.replace(',', '.')
+    }
+    bill = Number(bill)
+
+    if(bill == "" || bill <= 0){
+        alert("The value of the bill can't be zero")
+    }else if(amountOfPeople == "" || amountOfPeople <= 0){
+        alert("The number of people can't be zero")
+    }else{
+        let tipValue = Math.round(((bill * percentage) / amountOfPeople) * 100) / 100
+        let tipOutput = document.getElementById('output-tip')
+        tipOutput.innerHTML = `$${tipValue}`
+
+        let totalValue = bill / amountOfPeople + tipValue
+        let totalOutput = document.getElementById('output-total')
+        totalOutput.innerHTML = `$${totalValue}`
+    }
+}
+
+function resetCalculator(){
+    let form = document.getElementById('calculator-form').reset()
+    let tipOutput = document.getElementById('output-tip')
+    let totalOutput = document.getElementById('output-total')
+    tipOutput.innerHTML = `$0.00`
+    totalOutput.innerHTML = `$0.00`
+}
